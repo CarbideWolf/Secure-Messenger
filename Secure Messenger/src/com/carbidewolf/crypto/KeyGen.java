52ecc197e94393c40f5761f61f7e935907a98b10
CarbideWolf <Pyrostorm256@gmail.com>
2015-07-20 23:07:51 +0100

    Continue Work on KeyGen

--- Secure Messenger/src/com/carbidewolf/crypto/KeyGen.java
+++ Secure Messenger/src/com/carbidewolf/crypto/KeyGen.java
@@ -1,11 +1,23 @@
 package com.carbidewolf.crypto;
 
+import java.util.HashMap;
+
+import gnu.crypto.Registry;
+import gnu.crypto.key.IKeyPairGenerator;
+import gnu.crypto.key.KeyPairGeneratorFactory;
 import gnu.crypto.key.rsa.RSAKeyPairGenerator;
+import gnu.crypto.prng.IRandom;
+import gnu.crypto.prng.PRNGFactory;
 
 public class KeyGen
 {
 	public void GenerateKeyPair()
 	{
+		IKeyPairGenerator kpg = KeyPairGeneratorFactory.getInstance(Registry.RSA_KPG);
 		
+		IRandom rand = PRNGFactory.getInstance("MDGenerator");
+		
+		HashMap map = new HashMap();
+		//map.put(RSAKeyPairGenerator.SOURCE_OF_RANDOMNESS, value);
 	}
 }
