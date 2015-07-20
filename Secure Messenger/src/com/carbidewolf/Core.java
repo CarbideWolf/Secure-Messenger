98f42bd1136abeeb3b9bfd1019e6e19580995ea3
CarbideWolf <Pyrostorm256@gmail.com>
2015-07-20 18:44:13 +0100

    Code Cleanup

--- Secure Messenger/src/com/carbidewolf/Core.java
+++ Secure Messenger/src/com/carbidewolf/Core.java
@@ -1,9 +1,33 @@
 package com.carbidewolf;
 
+import java.awt.EventQueue;
+
+import com.carbidewolf.gui.EncryptionGUI;
+
+/**
+ * 
+ * @author CarbideWolf
+ *
+ */
+
 public class Core
 {
 	public static void main(String args[])
 	{
-		
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					EncryptionGUI frame = new EncryptionGUI();
+					frame.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
--- Secure Messenger/src/com/carbidewolf/gui/EncryptionGUI.java
+++ Secure Messenger/src/com/carbidewolf/gui/EncryptionGUI.java
@@ -1,47 +1,29 @@
 package com.carbidewolf.gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.Color;
-import java.awt.SystemColor;
 
 /**
- * TODO Annotate Class
- *
+ * 
  * @author Richousrick
  *
  */
-public class EncryptionGUI extends JFrame {
+
+@SuppressWarnings("serial")
+public class EncryptionGUI extends JFrame
+{
 
 	private JPanel contentPane;
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					EncryptionGUI frame = new EncryptionGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
-	public EncryptionGUI() {
+	public EncryptionGUI()
+	{
 		
 		Color contentColour = new Color(39,39,39);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
